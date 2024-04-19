8735c8e1c471268232a3be2c5b161df111e88ebd
jongas124 <kossos124@gmail.com>
2024-04-19 16:25:26 -0300

    Bairro

---
+++ tarefa07/Bairro.java
@@ -0,0 +1,27 @@
+package tarefa07;
+
+public class Bairro {
+    private String nome;
+    private Cidade cidade;
+
+    public Bairro(String nome, Cidade cidade) {
+        this.nome = nome;
+        this.cidade = cidade;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
