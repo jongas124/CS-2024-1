a384931aceee1c3f843500fa99b48bce72e7bd3a
jongas124 <kossos124@gmail.com>
2024-04-19 16:03:16 -0300

    Estado

--- tarefa07/Estado.java
+++ tarefa07/Estado.java
@@ -4,4 +4,17 @@ public class Estado {
     private String sigla;
     public String nome;
     public Pais pais;
+
+    public Estado(String sigla, String nome) {
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
 }
