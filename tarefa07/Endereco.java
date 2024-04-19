5172c4b56c287c4adebe56538276487a3ecd92e0
jongas124 <kossos124@gmail.com>
2024-04-19 15:59:49 -0300

    Pais

--- tarefa07/Endereco.java
+++ tarefa07/Endereco.java
@@ -4,4 +4,6 @@ public class Endereco {
     private int numero;
     private String complemento;
     private int CEP;
+    private TipoEndereco tipoEndereco;
+    private Bairro bairro;
 }
---
+++ tarefa07/Estado.java
@@ -0,0 +1,7 @@
+package tarefa07;
+
+public class Estado {
+    private String sigla;
+    public String nome;
+    public Pais pais;
+}
