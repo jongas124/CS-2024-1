3b6f01b28919725748609fe6edc50e50dffb854b
jongas124 <kossos124@gmail.com>
2024-04-19 16:50:20 -0300

    Endereco final

--- tarefa07/Endereco.java
+++ tarefa07/Endereco.java
@@ -6,4 +6,63 @@ public class Endereco {
     private int CEP;
     private TipoEndereco tipoEndereco;
     private Bairro bairro;
+    private Logradouro logradouro;
+
+    public Endereco(int numero, String complemento, int CEP, TipoEndereco tipoEndereco, Bairro bairro, Logradouro logradouro) {
+        this.numero = numero;
+        this.complemento = complemento;
+        this.CEP = CEP;
+        this.tipoEndereco = tipoEndereco;
+        this.bairro = bairro;
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public int getCEP() {
+        return CEP;
+    }
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public Bairro getBairro() {
+        return bairro;
+    }
+
+    public Logradouro getLogradouro() {
+        return logradouro;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public void setCEP(int cEP) {
+        CEP = cEP;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
+
+    public void setBairro(Bairro bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setLogradouro(Logradouro logradouro) {
+        this.logradouro = logradouro;
+    }
+
 }
